54fff87fbe7e62a7e6bcc94ad0f343353cb5e990
Rohan Saha <saharohan907@gmail.com>
2026-05-06 05:23:18 +0530

    Run migrations on Render deploy

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
 
-python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --no-input
