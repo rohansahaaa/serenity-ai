8f8518a487483266a687248cfe3e408fc59c3378
Rohan Saha <saharohan907@gmail.com>
2026-05-06 05:15:14 +0530

    Force migrations on deploy

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade pip
 pip install --no-cache-dir -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --no-input
