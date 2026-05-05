b0e1dc0bc05c6be2740a082f3d479db1e82eed62
Rohan Saha <saharohan907@gmail.com>
2026-05-06 05:03:58 +0530

    Run migrations during deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
